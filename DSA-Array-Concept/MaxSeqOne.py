bd25c7ceb95f744c0a325e446f7f40f14f479806
jayaparwar14 <jayaparwar14@gmail.com>
2026-09-12 18:11:18 +0530

    Add new file

---
+++ DSA-Array-Concept/MaxSeqOne.py
@@ -0,0 +1,25 @@
+class solution :
+
+    def maxseqone (self,nums) :
+
+        n = len(nums) 
+        count = 0
+        max_count = 1
+        
+
+        for i in range (0,n) :
+
+            if(nums[i] == 1) :
+
+                count += 1
+               
+            else :
+
+                max_count = max(max_count,count)
+                count = 0
+
+        return max_count
+
+nums = [1,1,0,0,0,1,1,1,1,1,0,1,0] 
+S = solution()
+print(S.maxseqone(nums))
